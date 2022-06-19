0cb64300487dfdbf010b9bd524c9f90ba88118ea
QFFS <lylyyhb@163.com>
2022-06-19 13:23:47 +0800

    Update rules.mk
    add bootloader config

--- rules.mk
+++ rules.mk
@@ -4,6 +4,7 @@ MCU_LDSCRIPT = STM32F103x8_0x08005800_bootloader
 BOARD = STM32_F103_STM32DUINO
 DFU_ARGS = -d 0483:df11 -a 0 -s 0x08005800
 DFU_SUFFIX_ARGS = -v 0483 -p DF11
+BOOTLOADER = custom
 # Build Options
 #   change yes to no to disable
 #
